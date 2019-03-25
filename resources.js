2ac83a6e413a3c1a9453ec8385b1ab5a2d6bb33c
dlousa92 <dlousa92@gmail.com>
2019-03-25 14:58:16 -0400

    create map and layers

--- resources.js
+++ resources.js
@@ -1,1 +1,12 @@
-console.log('hello')
+const map = L.map('map').setView([50, 15], 4)
+
+// Adding Voyager Basemap
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
+    maxZoom: 18
+}).addTo(map)
+
+// Adding Voyager Labels
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    zIndex: 10
+}),addTo(map)
